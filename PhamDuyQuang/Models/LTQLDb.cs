b93bc04efb62a346e0338628ee6ff200bad3da7e
DuyQuang1122 <77633978+DuyQuang1122@users.noreply.github.com>
2021-10-05 18:21:16 +0700

    quang

---
+++ PhamDuyQuang/Models/LTQLDb.cs
@@ -0,0 +1,26 @@
+using PhamDuyQuang.Models;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace PhamDuyQuang.Models
+{
+    public partial class LTQLDb : DbContext
+    {
+        public LTQLDb()
+            : base("name=LTQLDb")
+        {
+        }
+
+        public virtual DbSet<LopHoc> LopHocs { get; set; }
+        public virtual DbSet<SinhVien> SinhViens { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SinhVien>()
+                .Property(e => e.MaSinhVien)
+                .IsUnicode(false);
+        }
+    }
+}
